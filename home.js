e4926439e78c1d42b28d5ae9ae9a6c6710fe089c
bluhurr <bluhurr@gmail.com>
2022-06-19 15:13:56 -0400

    More styling and content added to landing page

---
+++ home.js
@@ -0,0 +1,22 @@
+const downArrow = document.getElementById('main-arrow');
+downArrow.addEventListener('click', (e)=>{
+  document.getElementById('home-about-content').scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+    inline: 'center'
+  });
+});
+
+const myNav = document.getElementById('navbar');
+
+window.onscroll = () => {
+  "use strict";
+  console.log(document.documentElement.scrollTop);
+  if(document.documentElement.scrollTop >= 20){
+    myNav.classList.add("nav-colored");
+    myNav.classList.remove("nav-transparent");
+  }else{
+    myNav.classList.add("nav-transparent");
+    myNav.classList.remove("nav-colored");
+  }
+};
